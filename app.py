0e5618bf1b72a5d92eba6b56e4d59b565bd5804a
MustafaBioS <bioshpere.voices@gmail.com>
2025-09-17 06:58:50 +0300

    done for today, i finished the main stuff and i'll continue polishing up the website tommorow

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_migrate import Migrate
+import os
+
+# Initialization
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'AudioVisualizer'
+
+
+
+# Routes
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template('home.html')
+    if request.method == 'POST':
+        songname = request.form.get('songname')
+        authorname = request.form.get('authorname')
+        audio = request.files['audio']
+
+        if audio and audio.filename.endswith('.mp3'):
+            filename = audio.filename
+            filepath = os.path.join('static/songuploads', filename)
+            audio.save(filepath)
+
+        session["song"] = {
+            "audio": filename, 
+            "songname": songname, 
+            "author": authorname}
+        
+        return redirect(url_for('player'))
+
+@app.route('/player')
+def player():
+    song = session.get('song')
+    if not song:
+        return redirect(url_for('home'))
+
+    return render_template('index.html', 
+                           songname=song['songname'], 
+                           author=song['author'],
+                           audio=song['audio']
+                           )
+
+# Run
+
+if __name__ == '__main__':
+    app.run(debug=True)
